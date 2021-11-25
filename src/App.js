2268adec7d76f8243817196d62b22e522625bc0c
Mritunjay Sinha <mritunjaysinha@Mritunjays-MacBook-Air.local>
2021-11-25 13:47:36 -0800

    development

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload . Boy tome
+          Edit <code>src/App.js</code> and save to reload . Boy tome - feature development
         </p>
         <a
           className="App-link"
